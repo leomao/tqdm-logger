62c1b24379f8f11286be8d33523e97982e040475
Leo Mao <leomaoyw@gmail.com>
2018-10-03 21:37:46 +0800

    make log updatable with udpate flag, use reset to freeze the log

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,27 +1,45 @@
+import random
 from tqdm import tqdm
 from tqdm import trange
 from time import sleep
-from tqdm_logger import logger
+import tqdm_logger as logger
 from tqdm_logger import seclog, log, atten, note, warning
 from tqdm_logger.ansistyle import stylize, fg, bg, attr, RESET
 
+
 def test_main():
     print(stylize('TESTING', attr('underlined')))
-    logger.info(stylize('TESTING', fg('green'), attr('underlined')))
+    logger.logger.info(stylize('TESTING', fg('green'), attr('underlined')))
     for i in trange(10):
-        logger.warning(f'TEST {i}')
-    logger.debug(atten(f'ENDING'))
-    logger.debug(123)
+        logger.logger.warning('TEST {}'.format(i))
+        sleep(0.1)
+    logger.logger.debug(atten('ENDING'))
+    logger.logger.debug(123)
 
     with tqdm(total=10) as tbar:
         for i in range(10):
-            seclog(['Graph', 'blue'], 123)
-            seclog(['Graph', 'blue'])
+            logger.seclog(['Graph', 'blue'], 123)
+            logger.seclog(['Graph', 'blue'])
             warning('this is a warning\nsomething wrong')
-            log('multiline logging:\nthis is a multiline string')
-            log(note('hao', 123))
+            logger.log('multiline logging:\nthis is a multiline string')
+            logger.log(note('hao', 123))
             sleep(0.2)
             tbar.update(1)
+
+    for i in trange(10):
+        ha = 'ha' * (9 - i)
+        logger.seclog(['Testing', 'red'], 'TEST: {}'.format(ha), update=True)
+        sleep(0.2)
+
+    # if use multiple updating log
+    logger.reset()
+    for i in trange(10):
+        ha = 'ha' * (9 - i)
+        value = random.random()
+        logger.seclog(['Multiline', 'yellow'],
+                      'something: {}\ncool: {}'.format(ha, value), update=True)
+        sleep(0.2)
+
 
 if __name__ == '__main__':
     test_main()
--- tqdm_logger/__init__.py
+++ tqdm_logger/__init__.py
@@ -1,26 +1,61 @@
-import logging
+import os
 import sys
+import logging
+from collections.abc import Sequence
 
+# must import before import tqdm
+import tqdm._utils as tqdm_utils
 from tqdm import tqdm
 from .ansistyle import stylize, fg, bg, attr, RESET
 
+
+def _get_cols():
+    return os.get_terminal_size().columns
+
+
 class DummyTqdmFile:
+    MOVE_UP = tqdm_utils._term_move_up()
+
     def __init__(self, fobj):
         self.fobj = fobj
+        self._updating = False
+        self.tmp_cnt = 0
+        self.line_cnt = 0
+
+    def updating(self):
+        self.tmp_cnt = 0
+        self._updating = True
+
+    def updated(self):
+        self._updating = False
+        self.line_cnt = self.tmp_cnt
+
+    def reset(self):
+        self.line_cnt = 0
 
     def write(self, msg):
+        cols = _get_cols()
+        msg = msg.ljust(cols)
+        if self._updating:
+            self.tmp_cnt += msg.count('\n')
+            if self.line_cnt > 0:
+                msg = DummyTqdmFile.MOVE_UP * self.line_cnt + msg
+        self.line_cnt = 0
         if '_instances' not in tqdm.__dict__ or len(tqdm._instances) == 0:
             self.fobj.write(msg)
         elif len(msg.rstrip()) > 0:
             tqdm.write(msg, file=self.fobj)
             self.fobj.flush()
 
+
+_output_fp = DummyTqdmFile(sys.stderr)
+
+
 logging.basicConfig(format='{asctime} - {message}',
                     datefmt='%H:%M:%S',
                     style='{',
                     level=logging.INFO,
-                    stream=DummyTqdmFile(sys.stderr)
-                    )
+                    stream=_output_fp)
 logger = logging.getLogger('root')
 
 
@@ -33,26 +68,49 @@ def note(*args):
                    attr('underlined'))
 
 
-def log(*args, attrs=None, prefix=None):
+def section_prefix(text, color=None):
+    if color is None:
+        return '[{}]'.format(text)
+    else:
+        return '{}[{}]{}'.format(fg(color), text, RESET)
+
+
+def reset():
+    _output_fp.reset()
+
+
+def log(*args, attrs=None, prefix=None, update=False):
     msg = ' '.join(str(x) for x in args)
     if attrs and len(attrs):
         styles = (attr(s) for s in attrs)
         msg = stylize(msg, *styles)
+
+    if update:
+        _output_fp.updating()
+
     for line in msg.split('\n'):
         if prefix:
-            line = f'{prefix} {line}'
+            line = '{} {}'.format(prefix, line)
         logger.warning(line)
 
+    if update:
+        _output_fp.updated()
 
-def seclog(section, *args, attrs=None):
-    if type(section) is list or type(section) is tuple:
-        section, color = section
-        section = '{}[{}]{}'.format(fg(color), section, RESET)
-    log(*args, attrs=attrs, prefix=section)
+
+def seclog(section, *args, attrs=None, update=False):
+    if isinstance(section, str):
+        section = section_prefix(section)
+    elif isinstance(section, Sequence):
+        section = section_prefix(*section)
+    else:
+        raise TypeError('the type of section is wrong!')
+    log(*args, attrs=attrs, prefix=section, update=update)
+    return section
 
 
 def warning(*args):
-    seclog(atten('[Warning]'), *args)
+    log(atten('[Warning]'), *args)
+
 
 __all__ = [
     'atten',
@@ -61,4 +119,5 @@ __all__ = [
     'seclog',
     'warning',
     'logger',
+    'reset',
 ]
